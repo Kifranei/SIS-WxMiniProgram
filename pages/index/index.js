82ca4213218e608e9dbfa650e907226c932cb054
Kifranei <xinckidn@outlook.com>
2026-05-12 22:33:18 +0800

    feat: refresh mini program UI

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,7 +1,6 @@
 const { buildApiUrl } = require('../../utils/api');
 const { getStoredUser } = require('../../utils/auth');
 
-// pages/index/index.js
 Page({
   data: {
     username: '',
@@ -16,16 +15,12 @@ Page({
       wx.reLaunch({ url: '/pages/main/main' });
     }
   },
-
-  // 监听用户名输入
   onUsernameInput: function(e) {
     this.setData({
       username: e.detail.value,
       errorMessage: ''
     });
   },
-
-  // 监听密码输入
   onPasswordInput: function(e) {
     this.setData({
       password: e.detail.value,
@@ -33,7 +28,6 @@ Page({
     });
   },
 
-  // 点击登录按钮时触发
   onLoginTap: function() {
     if (this.data.submitting) {
       return;
@@ -44,38 +38,32 @@ Page({
       return;
     }
 
-    // ## 这里是你后端 API 的地址 ##
-    // 请确保你的 Visual Studio 项目正在运行！
     const apiUrl = buildApiUrl('login');
     this.setData({
       submitting: true,
       errorMessage: ''
     });
 
-    // wx.request 是小程序发送网络请求的核心 API
     wx.request({
       url: apiUrl,
-      method: 'POST', // 我们的登录接口是 POST 请求
-      data: { // 这里是我们要发送给后端的数据
+      method: 'POST',
+      data: {
         Username: this.data.username,
         Password: this.data.password
       },
-      success: (res) => { // 请求成功的回调函数
+      success: (res) => {
         if (res.statusCode === 200) {
-          // 将用户信息保存到小程序的本地缓存中，方便其他页面使用
           wx.setStorageSync('userInfo', res.data);
 
-          // 跳转到主页 (我们假设主页叫 main)
           wx.reLaunch({
-            url: '/pages/main/main' // 你需要创建一个 main 页面
+            url: '/pages/main/main'
           });
 
         } else {
-          // 其他 HTTP 状态码，比如 401 (Unauthorized)
           this.setData({ errorMessage: '用户名或密码错误！' });
         }
       },
-      fail: (err) => { // 请求失败的回调函数
+      fail: (err) => {
         console.error('请求失败', err);
         this.setData({ errorMessage: '无法连接到服务器，请检查网络。' });
       },
